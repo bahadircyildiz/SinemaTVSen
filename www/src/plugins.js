064d9a00ff0f55d82c070e24cb1878bdd42e85fe
jaded <jaded@jadeds-Mac.local>
2017-09-17 04:21:55 +0300

    working copy tested on mac emulated ios. Ready to Testflight

--- www/src/plugins.js
+++ www/src/plugins.js
@@ -2,19 +2,20 @@ require('../lib/ionic/js/ionic.bundle.js');
 require('../lib/ionic-material/dist/ionic.material.js');
 
 document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  navigator.splashscreen.hide();
+
 
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
 
-  window.plugins.OneSignal
+  if(window.plugins.OneSignal){
+    // Enable to debug issues.
+    window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+    window.plugins.OneSignal
     .startInit("fdf274f5-d601-4b3f-acb0-475a246e317e")
     .handleNotificationOpened(notificationOpenedCallback)
     .endInit();
+  }
   
   // Call syncHashedEmail anywhere in your app if you have the user's email.
   // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
