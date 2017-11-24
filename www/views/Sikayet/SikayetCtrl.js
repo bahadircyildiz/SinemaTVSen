0c0219c154fb58094e0fd141f4f97807aac8f25f
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2017-11-25 01:19:24 +0300

    Made changes on sikayet page

--- www/views/Sikayet/SikayetCtrl.js
+++ www/views/Sikayet/SikayetCtrl.js
@@ -2,19 +2,6 @@ module.exports = function ($scope, $ionicModal, $ionicPopover, $ionicPopup, $sta
     // Form data for the login modal
     $scope.$parent.AuthService.redirectControl();
 
-    // var navIcons = document.getElementsByClassName('ion-navicon');
-    // for (var i = 0; i < navIcons.length; i++) {
-    //     navIcons.addEventListener('click', function () {
-    //         this.classList.toggle('active');
-    //     });
-    // }
-
-    // var fab = document.getElementById('fab');
-    // fab.addEventListener('click', function () {
-    //     //location.href = 'https://twitter.com/satish_vr2011';
-    //     window.open('https://twitter.com/satish_vr2011', '_blank');
-    // });
-
     // .fromTemplate() method
     var template = '<ion-popover-view>' +
                     '   <ion-header-bar>' +
